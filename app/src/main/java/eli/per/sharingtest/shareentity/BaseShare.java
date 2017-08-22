ca7696ed2e313fcbbf00772aa15b90d256fa375e
BoboHezi <bobohezi@outlook.com>
2017-08-22 16:59:33 +0800

    eli-20170822 更改修饰符

--- app/src/main/java/eli/per/sharingtest/shareentity/BaseShare.java
+++ app/src/main/java/eli/per/sharingtest/shareentity/BaseShare.java
@@ -7,8 +7,8 @@ import java.io.File;
 
 public abstract class BaseShare {
 
-    public Context context;
-    public Activity activity;
+    protected Context context;
+    protected Activity activity;
 
     public BaseShare(Context context, Activity activity) {
         this.context = context;
@@ -18,27 +18,27 @@ public abstract class BaseShare {
     /**
      * 初始化SDK
      */
-    public abstract void initSDK();
+    protected abstract void initSDK();
 
     /**
      * 分享图片
      *
      * @param imageFile
      */
-    public abstract void shareImage(File imageFile);
+    protected abstract void shareImage(File imageFile);
 
     /**
      * 分享视频
      *
      * @param videoFile
      */
-    public abstract void shareVideo(File videoFile);
+    protected abstract void shareVideo(File videoFile);
 
     /**
      * 分享图片组
      *
      * @param imageFiles
      */
-    public abstract void shareMultiImage(File imageFiles[]);
+    protected abstract void shareMultiImage(File imageFiles[]);
 
 }
